04e58a29255b0994b01336f509af09a96fdadaa0
mari4vitori4 <159458866+mari4vitori4@users.noreply.github.com>
2025-08-04 12:07:43 -0300

    innerHTML

---
+++ JS perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programacao',
+    'O que é função',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
